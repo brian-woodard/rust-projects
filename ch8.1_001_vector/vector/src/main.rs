bf57b8abf92a7f625b7aef411fe69563a4706c3e
sim <sim@advte.com>
2023-10-08 13:20:54 -0400

    ch8.2 Strings

--- ch8.1_001_vector/vector/src/main.rs
+++ ch8.1_001_vector/vector/src/main.rs
@@ -9,16 +9,16 @@ fn main() {
    // v.push(7);
    // v.push(8);
 
-   // let v = vec![1, 2, 3, 4, 5];
+   let v = vec![1, 2, 3, 4, 5];
 
-   // let third: &i32 = &v[2];
-   // println!("The third element is {third}");
+   let third: &i32 = &v[2];
+   println!("The third element is {third}");
 
-   // let third: Option<&i32> = v.get(3);
-   // match third {
-   //    Some(third) => println!("The third element is {third}"),
-   //    None => println!("There is no third element."),
-   // }
+   let seventh: Option<&i32> = v.get(6);
+   match seventh {
+      Some(seventh) => println!("The 7th element is {seventh}"),
+      None => println!("There is no 7th element."),
+   }
 
    // for i in &v {
    //    println!("Value in vec {i}");
@@ -30,11 +30,21 @@ fn main() {
       Text(String),
    }
 
-   let v = vec![
+   let mut v = vec![
       SpreadsheetCell::Int(3),
       SpreadsheetCell::Text(String::from("blue")),
       SpreadsheetCell::Float(10.12),
    ];
 
+   v.push(SpreadsheetCell::Int(5));
+
    println!("vector size {}", v.len());
+
+   for i in &v {
+      match i {
+         SpreadsheetCell::Int(value) => println!("Int val {value}"),
+         SpreadsheetCell::Text(value) => println!("Text val {value}"),
+         SpreadsheetCell::Float(value) => println!("Float val {value}"),
+      }
+   }
 }
